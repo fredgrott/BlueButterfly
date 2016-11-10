691fac2974494dcb139451d623ecfa716fa65e63
Fred Grott <fred.grott@gmail.com>
2016-11-10 09:05:25 -0600

    added googlenavdrawermenu

---
+++ core/src/main/java/com/github/shareme/bluebutterfly/core/widget/googlenavdrawermenu/views/CheckableImageView.java
@@ -0,0 +1,50 @@
+package com.github.shareme.bluebutterfly.core.widget.googlenavdrawermenu.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Checkable;
+
+/**
+ * Created by Arasthel on 15/04/14.
+ */
+public class CheckableImageView extends com.github.shareme.bluebutterfly.core.widget.ImageView implements Checkable {
+
+    private boolean checked = false;
+    private static final int[] CHECKED_STATE_SET = { android.R.attr.state_checked };
+
+    public CheckableImageView(Context context) {
+        super(context);
+    }
+
+    public CheckableImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CheckableImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public int[] onCreateDrawableState(int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked())
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+        return drawableState;
+    }
+
+    @Override
+    public void setChecked(boolean b) {
+        checked = b;
+        refreshDrawableState();
+    }
+
+    @Override
+    public boolean isChecked() {
+        return checked;
+    }
+
+    @Override
+    public void toggle() {
+        setChecked(!isChecked());
+    }
+}
