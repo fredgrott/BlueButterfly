3a296eceedce0534a31940686e27581d26f09347
Fred Grott <fred.grott@gmail.com>
2016-11-11 05:58:32 -0600

    switched mpagerslidingtabstrip to module

--- mpagerslidingtabstrip/src/main/java/com/github/shareme/bluebutterfly/mpagerslidingtabstrip/PagerSlidingTabStrip.java
+++ mpagerslidingtabstrip/src/main/java/com/github/shareme/bluebutterfly/mpagerslidingtabstrip/PagerSlidingTabStrip.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.github.shareme.bluebutterfly.core.widget.mpagerslidingtabstrip;
+package com.github.shareme.bluebutterfly.mpagerslidingtabstrip;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
